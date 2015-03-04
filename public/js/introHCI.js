de31cd6e294bbadd03dc4c303b423b871d088079
Molly Whitnack <mwhitnac@ucsd.edu>
2015-03-04 20:13:45 +0000

    hhh

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
 })
 
 function initializePage(){
-  $(".version_B_sessions").click(funcion(){ 
+  $("a.version_B_sessions").click(funcion(){ 
     woopra.track("version_B_sessions_click");
   })
-  $(".version_B_course").click(funcion(){ 
+  $("a.version_B_course").click(funcion(){ 
     woopra.track("version_B_course"); 
   })
-  $(".version_B_help").click(funcion(){
+  $("a.version_B_help").click(funcion(){
    woopra.track("version_B_help"); 
   })
-  $(".version_B_map").click(funcion(){ 
+  $("a.version_B_map").click(funcion(){ 
     woopra.track("version_B_map"); 
   })
 }
